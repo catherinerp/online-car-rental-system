559f375f9c7be77ebe66247b6c703aca6881ab98
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-05-02 11:41:35 +1000

    added foreach loop for all cars

--- components/browseCars.php
+++ components/browseCars.php
@@ -2,9 +2,12 @@
 <?php
     $filename = 'assets/cars.json';
     $data = file_get_contents($filename);
-    $decoded_json = json_decode($data)[0];
-    echo "<b>". $decoded_json->Name ." </b>";
-    echo $decoded_json->Model;
-    echo $decoded_json->Make;
-?>
+    $decoded_json = json_decode($data, true);
+   
+    foreach ($decoded_json as $car) {
+        echo "<b>" . $car['Name'] . "</b><br>";
+        echo "$" . $car['Price_per_day'] . "/day<br>";
+        echo "<img src='assets/images/car_images/" . $car['Image'] . "' style='height: 200px'></img><br><br>";
+    }
+?>    
 </p>
