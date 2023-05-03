46f181b8322224267042f0424d1481eedfb804b6
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-05-04 02:10:38 +1000

    added shopping cart hover

--- includes/header.php
+++ includes/header.php
@@ -21,13 +21,62 @@ session_start();
 <header class="header">
     <a href="./index.php"><img class="header-logo" src="assets/images/logo-full.png"></a>
     <div class="header-title">Car Rental Center</div>
-    <a href="viewCart.php"><button class="header-cart"><i class="fa fa-shopping-cart"></i></button></a>
+        <div class="nav-item dropdown">
+            <div class="dropdown">
+                <button class="header-cart" data-toggle="dropdown">
+                    <i class="fa fa-shopping-cart"></i>
+                    <?php 
+                    if (empty($_SESSION['cart'])) {
+                        echo "</button>";
+                    } else {
+                        $cart = $_SESSION['cart'];
+                        $count = count($cart);
+                        ?>
+                        <span style="font-size: 18px" class="badge badge-pill badge-danger"><?php echo $count?></span>
+                        </button>
+                        <div class="dropdown-menu">
+                            <?php
+                            $total_price = 0;
+                            foreach ($_SESSION['cart'] as $car_id => $car_data) {
+                                $car_name = $car_data['car_name'];
+                                $car_year = $car_data['car_year'];
+                                $car_image = $car_data['car_image'];
+                                $car_price = $car_data['car_price'];
+                                $car_price = number_format((float)$car_price, 2, '.', '');
+                                $quantity = $car_data['quantity'];
+                                ?>
+
+                                    <img src="assets/images/car_images/<?php echo $car_image?>" style="height:50px">
+                                    <a href="viewProduct.php?id=<?php echo $car_id?>"><?php echo $row['product_name']?></a>
+                                    <span class="price text-info">$<?php echo $car_price?></span> 
+                                    <span class="count">
+                                        <?php 
+                                        echo $quantity;
+                                        if ($quantity > 1) {
+                                            echo " days";
+                                        } else {
+                                            echo " day";
+                                        }?>
+                                    </span></br>
+                                <?php
+                                    $total_price += $car_price * $quantity;
+                            }
+                            ?>
+                        <hr>
+                        <p>Total: $<?php echo $total_price = number_format((float)$total_price, 2, '.', '');?></p>
+                        <a href="checkout.php" class="btn btn-primary btn-block">Checkout</a>
+                        <a href="viewCart.php" class="btn btn-primary btn-block">Cart</a>
+                        </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
     <div style="clear: both;"></div>
     <div class="topnav">
         <a href="./index.php">Home</a>
         <a class="account-button" href="#">Account</a>
         <a href="./help.php">Help</a>
     </div>
-
 </header>
 <body>
