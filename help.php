786cd7506a467cef11eaacf1b2310f4b72593e05
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-05-12 15:23:09 +1000

    added more information in help page

--- help.php
+++ help.php
@@ -3,13 +3,42 @@
     <div class="cart-container">
         </br>
         <h1 style="text-align:center">Welcome to Hertz-UTS!</h1>
+        <h3 style="text-align:center">About</h3>
         <p style="padding: 20px;width: 100%;">
         This is a online car rental center that allows you to browse our selection of cars
         and rent them for a certain number of days. The rental car is booked from the date
         of booking for the amount of days specified by the user. From the main browsing page,
         adding to the cart will mean it is booked for a day only - you must click on the car
-        to specify how many days you would like to book it for.
-        <p>
+        to specify how many days you would like to book it for. More details about the car can
+        be viewed by clicking 'Details' when hovering over the car.<br>
+        Details such as:
+        <ul>
+            <li>Make</li>
+            <li>Model</li>
+            <li>Year</li>
+            <li>Body Type</li>
+            <li>Mileage</li>
+            <li>Fuel Type</li>
+            <li>Transmission Type</li>
+            <li>Number of Seats</li>
+        </ul>
+        </p>
+        <h3 style="text-align:center">How to Book</h3>
+        <p style="padding: 20px;width: 100%;">
+        From the main browsing page, adding to the cart will mean it is booked for a day only
+         - you must click on the car details to specify how many days you would like to book it
+         for. You must provide all your billing and contact details as we will be in touch after
+         your booking has been made. Please wait to receive a confirmation email before visiting
+         our center to pick up the car.
+        </p>
+        <h3 style="text-align:center">Booking Rules</h3>
+        <p style="padding: 20px;width: 100%;">
+        As you checkout, the required payment amount will be displayed and it <b>must</b> be paid 
+        upfront. You are able to view your rental booking history under 'Account' and you can choose
+        to cancel it. Cancelling your booking does mean forfeiting your deposit. Cars are provided
+        with a full tank - on the last day of your booking, all rented vehicles must return in the
+        same condition with a full tank. Compensation for fuel costs will not be provided.
+        </p>
     </div>
 </div>
 <?php include 'includes/footer.php'; ?>
