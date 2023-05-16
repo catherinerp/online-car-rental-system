7c9bfa02693d2625dc26ca3e4c65c2181165a831
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-05-16 13:30:58 +1000

    changed database info again

--- includes/dbConfig.php
+++ includes/dbConfig.php
@@ -1,11 +1,10 @@
 <?php
-    $hostname = "containers-us-west-39.railway.app";
-    $username = "root";
-    $password = "uFMAJH8gJeM2BdoffXCL";
-    $dbname = "railway";
-    $port = "7845";
+    $hostname = "localhost";
+    $username = "catherine";
+    $password = "password1";
+    $dbname = "assignment2";
 
-    $conn = mysqli_connect($hostname, $username, $password, $dbname, $port);
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
     // $link = mysqli_connect("awseb-e-wekpd7ppkx-stack-awsebrdsdatabase-docmvdrwmlb2.cmgwtbm1ff9j.us-east-1.rds.amazonaws.com","catherine","password1","uts");
     if (!$conn)
         die("Could not connect to Server");
